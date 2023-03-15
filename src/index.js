f9db8bbd25f7e5997a6c59a0705b270d2e86e2b7
Tyler Randall <tyler7r@gmail.com>
2023-03-15 09:15:23 -0400

    Continued work and testing on gameboard factory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-function createGameBoard() {
+export let activeBoard = [];
+export function createBoard() { 
+    if (activeBoard.length >= 100) return;
+    for (let i = 1; i <= 100; i++) {
+        let square = {};
+        square.name = i;
+        square.status = 'untargeted';
+        activeBoard.push(square);
+    }
+    return activeBoard.length;
+};
 
-}
+createBoard();
 
 function Ship(length = 2, name = 'battleship') {
     return {
@@ -27,17 +37,40 @@ function Ship(length = 2, name = 'battleship') {
 }
 
 function Gameboard() {
+
     return {
-        placeShip([x, y]) {
-            let createdShip = new Ship();
-            createdShip.coordinates = [];
-            createdShip.coordinates.push({x, y});
-            createdShip.coordinates.push({x, y:y+1});
-            return createdShip.coordinates;
+        shipMap: {},
+
+        createShips() {
+            this.shipMap.destroyer = new Ship(2, 'destroyer');
+            this.shipMap.submarine = new Ship(3, 'submarine');
+            this.shipMap.cruiser = new Ship(3, 'cruiser');
+            this.shipMap.battleship = new Ship(4, 'battleship');
+            this.shipMap.carrier = new Ship(5, 'carrier');
+            return Object.keys(this.shipMap).length;
         },
 
-        receiveAttack() {
+        placeShip(ship, [x, y]) {
+            let size = ship.length;
+            ship.coordinates = [];
+            if (size + x > 10) return 'Invalid Location'
+            for (let i = 0; i < size; i++) {
+                ship.coordinates.push({x: x+i, y});
+                let index = ((y * 10) + (x + i)) - 11;
+                let square = activeBoard[index];
+                square.occupied = ship.name;
+            }
+            return ship.coordinates;
+        },
 
+        receiveAttack([x, y]) {
+            let squareIndex = ((y * 10) + x) - 11;
+            activeBoard[squareIndex].status = 'attacked';
+            let shipSelect = activeBoard[squareIndex].occupied;
+            let ship = this.shipMap[`${shipSelect}`];
+            ship.hits += 1;
+            ship.isSunk(ship.hits);
+            return activeBoard[squareIndex];
         }
     }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { ship, gameboard } from "./index";
+import { ship, gameboard, activeBoard, createBoard } from "./index";
 
 describe('Ship Factory Function Property Check', () => {
     test('Ship length recorded properly', () => {
@@ -37,8 +37,64 @@ describe('Ship Factory Function Method Check', () => {
 })
 
 describe('Gameboard Method Check', () => {
-    test('placeShip returns new ships coordinates', () => {
-        expect(gameboard.placeShip([3, 4])).toStrictEqual([{x:3, y:4}, {x:3, y:5}]);
+    test('createShips creates five ships and stores them in shipMap', () => {
+        expect(gameboard.createShips()).toBe(5);
+    })
+    describe('shipMap contains all five ships', () => {
+        test('Contains destroyer', () => {
+            expect(gameboard.shipMap.destroyer).toBeTruthy();
+        })
+        test('Contains submarine', () => {
+            expect(gameboard.shipMap.submarine).toBeTruthy();
+        })
+        test('Contains cruiser', () => {
+            expect(gameboard.shipMap.cruiser).toBeTruthy();
+        })
+        test('Contains battleship', () => {
+            expect(gameboard.shipMap.battleship).toBeTruthy();
+        })
+        test('Contains carrier', () => {
+            expect(gameboard.shipMap.carrier).toBeTruthy();        })
+    })
+    describe('placeShip returns new ships coordinates. Ship length and board size accounted for', () => {
+        test('destroyer coordinates correct', () => {
+            expect(gameboard.placeShip(gameboard.shipMap.destroyer, [3, 4])).toStrictEqual([{x:3, y:4}, {x:4, y:4}]);
+        })
+        test('carrier coordinates correct', () => {
+            expect(gameboard.placeShip(gameboard.shipMap.carrier, [5, 2])).toStrictEqual([{x: 5, y: 2}, {x: 6, y:2}, {x: 7, y: 2}, {x: 8, y: 2}, {x: 9, y: 2}]);
+        })
+        test('will not allow ship to placed off the 10x10 gameboard', () => {
+            expect(gameboard.placeShip(gameboard.shipMap.carrier, [8, 5])).toBe('Invalid Location');
+        })
+        test('After ship is placed, it should add a property to the associated squares with the name of the ship that is placed on it', () => {
+            expect(activeBoard[14].occupied).toBe('carrier');
+            expect(activeBoard[17].occupied).toBe('carrier');
+        })
+    })
+    describe('receiveAttack takes in coordinates and adjusts gameboard as necessary', () => {
+        test('receiveAttack returns which square was attacked', () => {
+            expect(gameboard.receiveAttack([4, 4])).toBe(activeBoard[33]);
+        })
+        test("receiveAttack updates the attacked squares's status", () => {
+            expect(activeBoard[33].status).toBe('attacked');
+        })
+        test('receiveAttack updates the number of hits on the ship locatd on the square', () => {
+            expect(gameboard.shipMap.destroyer.hits).toBe(1);
+        })
+        test('receiveAttack will update sunk status of ship if the last attack kills the ship', () => {
+            expect(gameboard.receiveAttack([3, 4])).toBe(activeBoard[32]);
+            expect(gameboard.shipMap.destroyer.sunk).toBeTruthy();
+        })
+        test('receiveAttack continued test', () => {
+            expect(gameboard.receiveAttack([5, 2])).toBe(activeBoard[14]);
+            expect(gameboard.shipMap.carrier.hits).toBe(1);
+            gameboard.receiveAttack([6, 2]);
+            gameboard.receiveAttack([7, 2]);
+            gameboard.receiveAttack([8, 2]);
+            expect(gameboard.shipMap.carrier.hits).toBe(4);
+            gameboard.receiveAttack([9, 2]);
+            expect(gameboard.shipMap.carrier.sunk).toBeTruthy();
+        })
     })
 })
 
